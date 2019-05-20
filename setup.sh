0c429ee7b1b35e21e49f90be1de9c75f87b4f70b
Nir Geller <gnir.work@gmail.com>
2019-05-20 20:33:50 +0300

    Added setup script and removed tmux configuration for a better one

---
+++ setup.sh
@@ -0,0 +1,35 @@
+set -x
+
+echo "Configurating git."
+git config --global user.email "gnir.work@gmail.com"
+git config --global user.name "Nir Geller"
+echo "Done"
+
+echo "Installing tmux..."
+sudo apt-get install tmux -y
+cd ~
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local
+cd -
+echo "Done"
+
+echo "Installing vim..."
+sudo apt-get install vim -y
+echo "Done"
+
+echo "Installing curl"
+sudo apt-get install curl -y
+echo "Done"
+
+echo "Installing zsh"
+sudo apt-get install zsh -y
+echo "Done"
+
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp .zshrc ~
+
+# Doing these will run zsh on terminal start (This is a patch but setting zsh to default shell doesn't work)
+echo zsh >> ~/.bashrc
+echo "Done"
