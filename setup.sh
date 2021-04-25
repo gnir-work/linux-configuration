6d0f94a9a84420ffce1cf7c8c99be74cbe6795b0
Nir Geller <gnir.work@gmail.com>
2021-04-26 01:02:46 +0300

    Make post_setup.sh executable.

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ echo "Done"
 
 echo "Installing pyenv..."
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-sudo apt install gcc build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev uuid-dev 
+sudo apt install gcc build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev uuid-dev -y
 echo "Done"
 
 echo "Installing vim..."
